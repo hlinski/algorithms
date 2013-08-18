c3fb7239b4d99133de5ef8f162dd6c5a204d0381
Alexander Hlinski <ahlinski@fitbit.com>
2013-08-18 18:31:35 +0300

    priority queue implementation

--- priority_queue.py
+++ priority_queue.py
@@ -1,33 +1,54 @@
 class PriorityQueue(object):
-	"""docstring for PriorityQueue"""
-	def __init__(self, array):
-		self.array = array
+    """docstring for PriorityQueue"""
 
-	def less(self,i, j):
-		if self.array[i] < self.array[j]:
-			return True
-		return False
+    def __init__(self, array):
+        self.array = array
 
-	def exch(self, i, j):
-		temp = self.array[i]
-		self.array[i] = self.array[j]
-		self.array[j] = temp
+    def less(self, i, j):
+        if self.array[i] < self.array[j]:
+            return True
+        return False
 
-	def swim(self, k):
-		while k > 1 and self.less(k/2, k):
-			self.exch(k/2, k)
-			k = k/2
+    def exch(self, i, j):
+        temp = self.array[i]
+        self.array[i] = self.array[j]
+        self.array[j] = temp
 
-	def sink():		
-		pass
+    def swim(self, k):
+        while k > 1 and self.less(k / 2, k):
+            self.exch(k / 2, k)
+            k = k / 2
 
-	def add(self, element):
-		array.append(element)
-		self.swim(len(array)-1)
+    def sink(self, k):
+        while 2 * k <= len(self.array):
+            j = 2 * k
+            if j < len(self.array)-1:
+                if self.less(j, j + 1):
+                    j = j + 1
+            if not self.less(k, j):
+                break
+            self.exch(k, j)
+            k = j
+
+    def add(self, element):
+        array.append(element)
+        self.swim(len(array) - 1)
+
+    def remove(self):
+        max = self.array[1]
+        self.exch(1, len(self.array) - 1)
+        del self.array[len(self.array) - 1]
+        self.sink(1)
+        return max
 
 
-array = [4,2,1]
+array = [-1]
 queue = PriorityQueue(array)
+queue.add(2)
 queue.add(3)
+queue.add(4)
+queue.add(5)
+queue.add(6)
 print array
-		
+print queue.remove()
+print array
